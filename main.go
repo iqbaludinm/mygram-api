c8a1d192efc93dcb2316ab3aa794bdafef7d76e7
Muhammad Iqbaludin Zaky <iqbalux@gmail.com>
2023-04-20 22:29:40 +0700

    setup for deploy

--- main.go
+++ main.go
@@ -3,17 +3,11 @@ package main
 import (
 	"github.com/iqbaludinm/mygram-api/app"
 	"github.com/iqbaludinm/mygram-api/config"
-	"github.com/joho/godotenv"
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
-
 	// init gorm
-	err = config.InitGorm()
+	err := config.InitGorm()
 	if err != nil {
 		panic(err)
 	}
